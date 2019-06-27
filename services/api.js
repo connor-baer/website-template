860d4c3b5e22d31df8fd59175b3fdd8c33308e0e
Connor Bär <connor.baer@sumup.com>
2019-06-27 12:19:52 +0200

    fix: Omit protocol on client

--- services/api.js
+++ services/api.js
@@ -10,10 +10,18 @@ function getHost(req = {}) {
   return req ? req.headers.host : '';
 }
 
+function getProtocol(req = {}) {
+  if (process.env.DEV) {
+    return 'http';
+  }
+  return req ? 'https' : '';
+}
+
 function fetchData(fullUrl) {
   return fetch(fullUrl)
     .then(r => r.json())
     .catch(error => {
+      // eslint-disable-next-line no-console
       console.error(error);
       return { error };
     });
@@ -24,7 +32,7 @@ export function fetchNotionPage(ctx = {}) {
   const { query = {} } = ctx;
   const { post: pageId } = query;
   const host = getHost(ctx.req);
-  const protocol = process.env.DEV ? 'http' : 'https';
+  const protocol = getProtocol(ctx.req);
   const pathname = `${API_BASEURL}/page`;
   const fullUrl = url.format({ protocol, host, pathname, query: { pageId } });
   return fetchData(fullUrl);
